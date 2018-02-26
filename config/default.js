2b2791cd53595b71ce997fbe804126f3809a2d16
Jacky <295587514@example.com>
2018-02-26 23:53:04 +0800

    写改80端口

--- config/default.js
+++ config/default.js
@@ -5,11 +5,11 @@
 // mongodb: mongodb 的地址，以 mongodb:// 协议开头，myblog 为 db 名
 
 module.exports = {
-    port: 80,
-    session: {
-      secret: 'myblog',
-      key: 'myblog',
-      maxAge: 2592000000
-    },
-    mongodb: 'mongodb://localhost:27017/myblog'
-  }
+  port: 80,
+  session: {
+    secret: 'myblog',
+    key: 'myblog',
+    maxAge: 2592000000
+  },
+  mongodb: 'mongodb://localhost:27017/myblog'
+}
